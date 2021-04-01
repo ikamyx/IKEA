93f7f46c510011b1a255e0153fc9ae695102e110
ikamyx <ikammmy@gmail.com>
2021-04-01 08:51:43 -0400

    commit04

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ document.querySelectorAll('.productCarousel li > a:first-child').forEach(d => {
     })
 })
 
-document.querySelector('#productDetail figure a').addEventListener('click', d => {
+document.querySelector('#productDetail figure a').addEventListener('click', () => {
     document.body.classList.remove('productExpand');
+})
+
+document.querySelector('#navButton').addEventListener('click', function(e) {
+    document.querySelector("#topNav ul").classList.add('show');
+    document.body.classList.add('productExpand');
+})
+
+document.querySelector("#topNav nav li:last-child").addEventListener('click', function(e) {
+    document.querySelector("#topNav ul").classList.remove('show');
+    document.body.classList.remove('productExpand');
+    e.stopPropagation();
 })
